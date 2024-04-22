d0b79465bfff85ca5a07e4655a9206359bedd880
TaskManagement <surajkushwaha8272@gmail.com>
2024-04-22 12:47:38 +0530

    Update

--- src/Components/SkillMaster.jsx
+++ src/Components/SkillMaster.jsx
@@ -14,13 +14,13 @@ function SkillMaster() {
                 </div>
 
                 <div className='flex w-[70%] h-auto flex-col '>
-                    <div className='flex flex-row 
+                    <div className='flex md:flex-row  flex-col
                       rounded-xl 
                       shadow-gray-500 shadow-lg bg-gray-200 '>
                         <div className='bg-goldenrod px-10 py-7  '>
                             <h1 className='text-5xl font-bold items-center text-center pt-2'>01</h1>
                         </div>
-                        <div className='px-10 py-7 w-[90%]'>
+                        <div className='md:px-10 px-5 py-7 w-[90%]'>
                             <h1 className='text-black text-2xl pb-3 font-bold items-center text-center'>Captivating Dashboards</h1>
                             <p>Turn raw data into actionable insights with captivating
                                 dashboards and reports.
@@ -28,26 +28,26 @@ function SkillMaster() {
                         </div>
                     </div>
 
-                    <div className='flex flex-row 
+                    <div className='flex md:flex-row  flex-col 
                       rounded-xl  mt-8 bg-gray-200
                       shadow-gray-500 shadow-lg '>
                         <div className='bg-goldenrod px-10 py-7'>
                             <h1 className='text-5xl font-bold items-center text-center pt-2'>02</h1>
                         </div>
-                        <div className='px-10 py-7 w-[90%]'>
+                        <div className='md:px-10 px-5 py-7 w-[90%]'>
                             <h1 className='text-black text-2xl pb-3 font-bold items-center text-center pl-50'>Data Manipulation</h1>
                             <p>Master importing, transforming, cleaning, and refining data for analysis
                             </p>
                         </div>
                     </div>
 
-                    <div className='flex flex-row 
+                    <div className='flex md:flex-row  flex-col
                       rounded-xl  mt-8 bg-gray-200
                       shadow-gray-500 shadow-lg '>
                         <div className='bg-goldenrod px-10 py-7'>
                             <h1 className='text-5xl font-bold items-center text-center pt-3'>03</h1>
                         </div>
-                        <div className='px-10 py-4 w-[90%]'>
+                        <div className='md:px-10 px-5 py-4 w-[90%]'>
                             <h1 className='text-black text-2xl pb-3 font-bold items-center text-center '>DAX Proficiency</h1>
                             <p>Conquer complex calculations and supercharge 
                                 data models with DAX (Data Analysis Expressions).
@@ -56,26 +56,26 @@ function SkillMaster() {
                         </div>
                     </div>
 
-                    <div className='flex flex-row 
+                    <div className='flex md:flex-row  flex-col
                       rounded-xl  mt-8 bg-gray-200
                       shadow-gray-500 shadow-lg '>
                         <div className='bg-goldenrod px-10 py-7'>
                             <h1 className='text-5xl font-bold items-center text-center'>04</h1>
                         </div>
-                        <div className='px-10 py-6 w-[90%]'>
+                        <div className='md:px-10 px-5 py-6 w-[90%]'>
                             <h1 className='text-black text-2xl pb-3 font-bold items-center text-center'>Data Security</h1>
                             <p>Fortify your data fortress with robust security measures and seamless sharing capabilities.
                             </p>
                         </div>
                     </div>
 
-                    <div className='flex flex-row 
+                    <div className='flex md:flex-row  flex-col
                       rounded-xl mt-8 bg-gray-200 
                       shadow-gray-500 shadow-lg '>
-                        <div className='bg-goldenrod px-10 py-7'>
+                        <div className='bg-goldenrod px-10  py-7'>
                             <h1 className='text-5xl font-bold items-center text-center pt-3'>05</h1>
                         </div>
-                        <div className='px-10 py-5 w-[90%]'>
+                        <div className='md:px-10 px-5 py-5 w-[90%]'>
                             <h1 className='text-black text-2xl pb-3 font-bold items-center text-center'>Performance Optimization</h1>
                             <p>Turbocharge performance and troubleshooting with precision.
 Integration Mastery: Seamlessly integrate Power BI with other Microsoft tools for a cohesive data ecosystem.
